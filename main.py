1d6de05b18a3a34faca8df100a151816e20eab82
Angel Higueros <angel.higueros@icloud.com>
2022-10-15 17:29:52 -0600

    feat(sr1): sr1

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Render(object):
         self.viewport_y = 0 
         self.viewport_width = 100 
         self.viewport_height = 100
-        self.current_color = color(0, 0, 0) # por defecto negro
+        self.current_color = color(255, 255, 255) # por defecto blanco
         self.vertex_color = color(200, 0, 0) # por defecto rojo
         self.framebuffer = []
         self.glClear()
@@ -39,40 +39,49 @@ class Render(object):
         self.height = height
 
     def glViewPort(self, x, y, width, height):
-        self.viewport_x = y
-        self.viewport_y = width
-        self.viewport_width = width
-        self.viewport_height = height
+
+        if self.width < x + width or self.height < y + height:
+            print("[!] El viewport debe estar dentro de las medidas de la pantalla")
+            self.viewport_x = 0
+            self.viewport_y = 0
+            self.viewport_width = self.width
+            self.viewport_height = self.height
+            self.glClear()
+        else:
+            self.viewport_x = x
+            self.viewport_y = y
+            self.viewport_width = width
+            self.viewport_height = height
+            self.glClear()
 
     def glClear(self):
         self.framebuffer= [
-            [self.current_color for x in range(self.width)]
+            [color(0, 0, 0) for x in range(self.width)]
             for y in range(self.height)
         ]
 
+        for x in range(self.width):
+            for y in range(self.height):
+                if x >= self.viewport_x and x <= self.viewport_width and y >= self.viewport_y and y <= self.viewport_height:
+                    self.framebuffer[x][y] = self.current_color 
+
     def glClearColor(self, r, g, b):
-        if r in range(0, 1) and g in range(0, 1) and b in range(0, 1): 
-            self.current_color = color(r, g, b)
-        else:
-            print("[!][glClearColor] Los valores del color rgb deben estar entre  0 y 1")
+        self.current_color = color(r, g, b)
 
     def glVertex(self, x, y):
 
         # convertir coordenadas normalizadas a cordenadas del dispositivo
-        half_size_width = self.width / 2
-        half_size_height = self.height / 2
+        half_size_width = self.viewport_width / 2
+        half_size_height = self.viewport_height / 2
 
-        coord_x = int((( x + 1 ) * half_size_width  ) + self.viewport_x)
-        coord_y = int((( y + 1 ) * half_size_height ) + self.viewport_y)
+        coord_x = int((( x + 1 ) * half_size_width ))
+        coord_y = int((( y + 1 ) * half_size_height ))
 
-        self.framebuffer[x][y] = self.vertex_color
+        self.framebuffer[coord_x][coord_y] = self.vertex_color
 
 
     def glColor(self, r, g, b):
-        if r in range(0, 1) and g in range(0, 1) and b in range(0, 1): 
-             self.vertex_color = color(r, g, b)
-        else:
-            print("[!][glColor] Los valores del color rgb deben estar entre  0 y 1")
+        self.vertex_color = color(r, g, b)
 
     def glFinish(self):
         f = open(self.filename, 'bw')
@@ -107,11 +116,11 @@ class Render(object):
 
 # IMPLEMENTACION
 r = Render()
-r.glInit('sr1-point.bpm')
+r.glInit('sr1-point.bmp')
 r.glCreateWindow(1024, 1024)
 r.glViewPort(50,50, 900, 900)
+r.glClearColor(255, 0, 0)
 r.glClear()
-r.glClearColor(1, 0, 0)
-r.glVertex(100, 100)
+r.glVertex(1, 1)
 r.glColor(1, 1, 1)
 r.glFinish()
